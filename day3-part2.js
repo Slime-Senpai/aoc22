dff2f904411cd090162ffe95bc756dbdc21d0d5b
Slime-Senpai <44924877+Slime-Senpai@users.noreply.github.com>
2022-12-07 23:23:30 +0100

    Create day3-part2.js

---
+++ day3-part2.js
@@ -0,0 +1,1 @@
+inputString.split(/([a-zA-Z]+\n[a-zA-Z]+\n[a-zA-Z]+)\n/).filter(a => a !== '').map(e => e.split('\n')[0].split('').map(f => e.split('\n')[1].includes(f) && e.split('\n')[2].includes(f) ? 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(f) + 1 : 0 ).find(a => a !== 0)).reduce((a, b) => a + b, 0)
