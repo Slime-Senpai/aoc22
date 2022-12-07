efa562e0a9dfa3d732fedfdb326bfd98814fee9d
Slime-Senpai <44924877+Slime-Senpai@users.noreply.github.com>
2022-12-07 23:23:13 +0100

    Create day3-part1.js

---
+++ day3-part1.js
@@ -0,0 +1,1 @@
+inputString.split('\n').map(e => e.slice(0, e.length/2).split('').map(f => e.slice(e.length/2).includes(f) ? 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(f) + 1 : 0 ).find(a => a !== 0)).reduce((a, b) => a + b, 0)
